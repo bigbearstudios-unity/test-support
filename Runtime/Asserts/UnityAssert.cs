51b4a5a1024bf19c8015522385cb63f3cc42c177
Stuart Farnaby <stuart@bigbearstudios.co.uk>
2020-08-08 10:10:07 +0100

    Added basic package.json entries

---
+++ Runtime/Asserts/UnityAssert.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using NUnit.Framework;
+using System;
+
+namespace BBUnity.TestSupport {
+    public static class UnityAssert {
+        public static void AreEqual(Vector2 v, Vector2 v2, float tolerance = 0.001f) {
+            float distance = Vector2.Distance(v, v2);
+            Assert.That(distance, Is.LessThanOrEqualTo(tolerance), AssertMessage(v, v2, distance, tolerance));
+        }
+
+        public static void AreEqual(Vector3 v, Vector3 v2, float tolerance = 0.001f) {
+            float distance = Vector2.Distance(v, v2);
+            Assert.That(distance, Is.LessThanOrEqualTo(tolerance), AssertMessage(v, v2, distance, tolerance));
+        }
+
+        public static void AreEqual(float f, float f2, float tolerance = 0.001f) {
+            float absolute = Math.Abs(f - f2);
+            Assert.That(absolute, Is.LessThanOrEqualTo(tolerance), AssertMessage(f, f2, absolute, tolerance)); 
+        }
+
+        private static string AssertMessage(Vector3 v, Vector3 v2, float distance, float tolerance) {
+            return String.Format("Expected: Vector3({0}, {1}, {2})\nBut was:  Vector3({3}, {4}, {5})\nDistance: {6} is greated than allowed delta {7}",
+                                    v.x, v.y, v.z,
+                                    v2.x, v2.y, v2.z,
+                                    distance, tolerance);
+        }
+
+        private static string AssertMessage(float f, float f2, float distance, float tolerance) {
+            return String.Format("Expected: float({0})\nBut was:  float({1})\nAbsolute: {2} is greated than allowed delta {3}",
+                                    f, f2, distance, tolerance);
+        }
+    }
+}
---
+++ Runtime/Extensions/DictionaryExtensions.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+
+using System;
+using System.Collections.Generic;
+
+namespace BBUnity.TestSupport {
+    public static class DictionaryExtensions {
+        public static void AreEqualToString(this Dictionary<string, int> dictionary, Func<int, string> action) {
+            foreach (KeyValuePair<string, int> entry in dictionary) {
+                Assert.AreEqual(entry.Key, action(entry.Value));
+            }
+        }
+
+        public static void AreEqualToInt(this Dictionary<int, int> dictionary, Func<int, int> action) {
+            foreach (KeyValuePair<int, int> entry in dictionary) {
+                Assert.AreEqual(entry.Key, action(entry.Value));
+            }
+        }
+    }
+}
