5e1b792a9ee20eb31c8ced20d83fddeaa1ef8ba7
Stuart Farnaby <stuart@bigbearstudios.co.uk>
2020-08-10 21:52:26 +0100

    Added ArrayExtensions with IsTrue / IsFalse / IsNull / IsNotNull methods

---
+++ Runtime/Extensions/ArrayExtensions.cs
@@ -0,0 +1,60 @@
+﻿using NUnit.Framework;
+
+using System;
+
+namespace BBUnity.TestSupport {
+    public static class ArrayExtensions {
+
+        /// <summary>
+        /// Helper method to allow an array of items to be ran through a Func and 
+        /// the result will have IsTrue called on it
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="array"></param>
+        /// <param name="action"></param>
+        public static void IsTrue<T>(this T[] array, Func<T, bool> action) {
+            foreach(T value in array) {
+                Assert.IsTrue(action(value));
+            }
+        }
+
+        /// <summary>
+        /// Helper method to allow an array of items to be ran through a Func and 
+        /// the result will have IsFalse called on it
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="array"></param>
+        /// <param name="action"></param>
+        public static void IsFalse<T>(this T[] array, Func<T, bool> action) {
+            foreach(T value in array) {
+                Assert.IsFalse(action(value));
+            }
+        }
+
+        /// <summary>
+        /// Helper method to allow an array of items to be ran through a Func and 
+        /// the result will have IsNotNull called on it
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="array"></param>
+        /// <param name="action"></param>
+        public static void IsNotNull<T>(this T[] array, Func<T, object> action) {
+            foreach(T value in array) {
+                Assert.IsNotNull(action(value));
+            }
+        }
+
+        /// <summary>
+        /// Helper method to allow an array of items to be ran through a Func and 
+        /// the result will have IsNull called on it
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="array"></param>
+        /// <param name="action"></param>
+        public static void IsNull<T>(this T[] array, Func<T, object> action) {
+            foreach(T value in array) {
+                Assert.IsNull(action(value));
+            }
+        }
+    }
+}
---
+++ Tests/Runtime/Extensions/ArrayExtensionsTests.cs
@@ -0,0 +1,48 @@
+﻿using NUnit.Framework;
+
+using BBUnity.TestSupport;
+
+namespace Extensions {
+    public class ArrayExtensionsTests {
+
+        private bool ReturnTrue(int v) {
+            return true;
+        }
+
+        private bool ReturnFalse(int v) {
+            return false;
+        }
+
+        [Test]
+        public void IsTrue_ShouldPass_IfAllItemsReturnTrue() {
+            new int[] {
+                1,2
+            }.IsTrue(ReturnTrue);
+        }
+
+        [Test]
+        public void IsTrue_ShouldThrowAssertionException_IfAllItemsReturnFalse() {
+            Assert.Throws(typeof(AssertionException), () => {
+                new int[] {
+                    1,2
+                }.IsTrue(ReturnFalse);
+            });
+        }
+
+        [Test]
+        public void IsFalse_ShouldPass_IfAllItemsReturnFalse() {
+            new int[] {
+                1,2
+            }.IsFalse(ReturnFalse);
+        }
+
+        [Test]
+        public void IsFalse_ShouldThrowAssertionException_IfAllItemsReturnTrue() {
+            Assert.Throws(typeof(AssertionException), () => {
+                new int[] {
+                    1,2
+                }.IsFalse(ReturnTrue);
+            });
+        }
+    }
+}
